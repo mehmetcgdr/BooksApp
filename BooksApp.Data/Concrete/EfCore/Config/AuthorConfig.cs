804412ee30c297a35cea376f801f1c3f9fc80ab1
Mehmet ÖÇGÜDER <met.ocguder@gmail.com>
2023-03-27 12:36:50 +0300

    author and image configs added

--- BooksApp.Data/Concrete/EfCore/Config/AuthorConfig.cs
+++ BooksApp.Data/Concrete/EfCore/Config/AuthorConfig.cs
@@ -21,12 +21,11 @@ namespace BooksApp.Data.Concrete.EfCore.Config
             builder.Property(x => x.Gender).IsRequired();
 
             builder.HasData(
-                new Author { Id = 1, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Name = "Orhan Parasaçan", Gender = "E", Url="orhan-parasacan" },
-                new Author { Id = 2, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Name = "Selami Gülgeçen", Gender = "E", Url="selami-gulgecen" },
-                new Author { Id = 3, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Name = "Seyhan Yolagelen", Gender = "E", Url="seyhan-yolagelen" },
-                new Author { Id = 4, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Name = "Hale Çokseven",  Gender = "E",Url="hale-cokseven" },
-                new Author { Id = 5, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Name = "Kemal Devabulan", Gender = "E",Url="kemal-devabulan" },
-                new Author { Id = 6, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Name = "Selen Günebakan", Gender = "E",Url="selen-gunebakan" }
+                new Author { Id = 1, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Name = "Saniye Bencik Kangal", Gender = "K", Url = "saniye-bencik-kangal" },
+                new Author { Id = 1, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Name = "Özge Naz", Gender = "K", Url = "ozge-naz" },
+                new Author { Id = 1, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Name = "James Clear", Gender = "E", Url = "james-clear" },
+                new Author { Id = 1, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Name = "İlber Ortaylı", Gender = "E", Url = "ilber-ortaylı" }
+
             );
         }
     }
--- BooksApp.Data/Concrete/EfCore/Config/ImageConfig.cs
+++ BooksApp.Data/Concrete/EfCore/Config/ImageConfig.cs
@@ -22,19 +22,11 @@ namespace BooksApp.Data.Concrete.EfCore.Config
             builder.Property(x => x.Url).IsRequired().HasMaxLength(500);
 
             builder.HasData(
-                new Image { Id = 1, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Url = "1.jpg", BookId = 1 },
-                new Image { Id = 2, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Url = "2.jpg", BookId = 2 },
-                new Image { Id = 3, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Url = "3.jpg", BookId = 3 },
-                new Image { Id = 4, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Url = "4.jpg", BookId = 4 },
-                new Image { Id = 5, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Url = "5.jpg", BookId = 5 },
-                new Image { Id = 6, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Url = "6.jpg", BookId = 6 },
-                new Image { Id = 7, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Url = "7.jpg", BookId = 7 },
-                new Image { Id = 8, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Url = "8.jpg", BookId = 8 },
-                new Image { Id = 9, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Url = "9.jpg", BookId = 9 },
-                new Image { Id = 10, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Url = "10.jpg", BookId = 10 },
-                new Image { Id = 11, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Url = "11.jpg", BookId = 11 },
-                new Image { Id = 12, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Url = "12.jpg", BookId = 12 },
-                new Image { Id = 13, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Url = "222.jpg", BookId = 1 }
+                new Image { Id = 1, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Url = "atomik-aliskanliklar.jpg", BookId = 1 },
+                new Image { Id = 1, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Url = "bronz-kutulu.jpg", BookId = 2 },
+                new Image { Id = 1, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Url = "dakikalar-icinde-ataturk-ve-dunyasi.jpg", BookId = 3 },
+                new Image { Id = 1, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now, IsApproved = true, Url = "zorbaliga-karsi-taktiklerim-var.jpg", BookId = 4 }
+
 
                 );
         }
